2cb15a822b139ab3117d29340d82b1f0df4b88a0
Joan Mayer Campbell <joanmayercampbell@gmail.com>
2016-06-17 16:43:44 -0400

    first attempt

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -8,8 +8,91 @@ namespace SummerSchool
 {
     class Program
     {
+        static string[] Students = new string[15];
+        static int[] Fees = new int[15];
+
+        static int DisplayMenuGetInput()
+        {
+            Console.Clear();
+
+            int menuChoice = 0;
+
+            Console.WriteLine("SUMMER SCHOOL ENROLLMENT SYSTEM");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Enroll a student");
+            Console.WriteLine("2 - Unenroll a student");
+            Console.WriteLine("3 - Print list of enrolled students");
+            Console.WriteLine("4 - Exit");
+            Console.WriteLine("");
+            Console.WriteLine("Enter option:");
+
+            menuChoice = Convert.ToInt32(Console.ReadLine());
+            return menuChoice;
+        }
+
+        static void EnrollStudent()
+        {
+            return;
+        }
+
+        static void UnenrollStudent()
+        {
+            return;
+        }
+
+        static void PrintStudents()
+        {
+            return;
+        }
+
+        static int NumOfEnrolled()
+        {
+            int numberOfStudents = 0;
+            int i = 0;
+            for (i,i < Student.Length;i++)
+            {
+                if Students[i] == null
+                    {
+                    numberOfStudents +=;
+                }
+            }
+            return numberOfStudents;
+        }
+
+
         static void Main(string[] args)
         {
+            int menuChoice = 0;
+
+            while (menuChoice !=4)
+            {
+                menuChoice = DisplayMenuGetInput();
+
+                if (menuChoice == 1)
+                {
+                    EnrollStudent();
+                }
+
+                if (menuChoice == 2)
+                {
+                    UnenrollStudent();
+                }
+
+                if (menuChoice == 3)
+                {
+                    PrintStudents(); 
+                }
+
+                if (menuChoice == 4)
+                {
+                    continue;
+                }
+                    
+            }
+            
+           
+            Console.ReadKey();
+
         }
     }
 }
